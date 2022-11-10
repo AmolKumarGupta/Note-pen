6db6fe924af4a7fd255ed954687216894f578b84
amol <amlo1232001@gmail.com>
2022-11-10 08:02:34 +0530

    fix: #3 issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar(){
         <>
         <nav className="bg-hard text-mixed border-[1px] border-hard rounded-sm lg:px-0 lg:fixed lg:h-full lg:w-[280px]">
             <h1 className="text-light text-lg font-bold py-3 px-2 lg:px-4 border-b-[1px] rounded-sm">Note pen <span onClick={handleToggle} className="float-right lg:hidden">0</span></h1>
-            <ul className={`px-2 lg:px-0 h-0 overflow-hidden ${ state?'h-full':'' } lg:h-full`}>
+            <ul className={`px-2 lg:px-0 overflow-hidden ${ state?'h-full':'h-0' } lg:h-full`}>
                 <li className="px-2 lg:px-4 py-2 hover:text-front hover:cursor-pointer ">Scrapper</li>
                 <li className="px-2 lg:px-4 py-2 hover:text-front hover:cursor-pointer nav_active">Test</li>
                 <li className="px-2 lg:px-4 py-2 hover:text-front hover:cursor-pointer">qweuowqeuoieu wqeoui</li>
